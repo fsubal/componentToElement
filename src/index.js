610b59894a81f069dbae8a8e9c5d9d05ced8684c
fsubal <fsubal@users.noreply.github.com>
2017-06-16 00:24:09 +0900

    Create index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import ReactDOM from 'react-dom/server';
 
+/**
+ * you can do something like this.
+ *
+ * componentToElement(() => <div>Hello World</div>);
+ * componentToElement(<SomeComponent foo={1} />);
+ *
+ * @param { React.Component } component
+ * @return { HTMLElement }
+ */
 export const componentToElement = (Component) => {
   const t = document.createElement('template');
   t.innerHTML = ReactDOM.renderToStaticMarkup(<Component />);
